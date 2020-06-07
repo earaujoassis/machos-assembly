77ac246fed2fedcaa08fadc1905761932ad40962
Carlos A <593456+earaujoassis@users.noreply.github.com>
2020-06-07 20:56:21 -0300

    SYS_write a new line (linefeed) for the hello world example

--- src/write_syscall.asm
+++ src/write_syscall.asm
@@ -1,9 +1,5 @@
 global    _main
 
-section   .data
-message:
-    db        "Write call!", 10
-
 section   .text
 _main:
     mov       rax, 0x02000004 ; SYS_write setup (SYS_write at 4 bits offset from 0x02000000)
@@ -11,6 +7,19 @@ _main:
     mov       rsi, message    ; SYS_write argument
     mov       rdx, 11         ; SYS_write argument size (bytes)
     syscall                   ; perform SYS_write
-    mov       rax, 0x02000001 ; SYS_exit setup
+
+    mov       rax, 0x02000004 ; SYS_write setup (SYS_write at 4 bits offset from 0x02000000)
+    mov       rdi, 1          ; SYS_write use STDOUT
+    mov       rsi, new_line   ; SYS_write argument
+    mov       rdx, 1          ; SYS_write argument size (bytes)
+    syscall                   ; perform SYS_write
+
+    mov       rax, 0x02000001 ; SYS_exit setup  (SYS_write at 1 bit  offset from 0x02000000)
     xor       rdi, rdi        ; SYS_exit argument
     syscall                   ; perform SYS_exit
+
+section   .data
+message:
+    db        "Write call!", 10
+new_line:
+    db        0xA
